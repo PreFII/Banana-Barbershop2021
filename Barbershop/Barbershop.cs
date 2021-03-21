b67c40cec80c99a15ea8027ab33aa60473d622ca
Mike <20vampir11@gmail.com>
2021-03-21 17:02:55 +0300

    added methods to add customers and services

--- Barbershop/Barbershop.cs
+++ Barbershop/Barbershop.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace Barbershop
 {
-    class Barbershop
+    static class Barbershop
     {
+        public static List<Customer> customers = new List<Customer>();
+        public static List<Service> services = new List<Service>();
+
+        public static int find_customer(Customer customer)
+        {
+            for (int i = 0; i < customers.Count; i++)
+            {
+                if (customer.is_compare(customers[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int find_service(Service service)
+        {
+            for (int i = 0; i < services.Count; i++)
+            {
+                if (service.is_compare(services[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static bool add_customer(Customer customer)
+        {
+            int customer_index = find_customer(customer);
+            if(customer_index != -1)
+            {
+                return false;
+            }
+
+            customers.Add(customer);
+            return true;
+        }
+
+        public static bool add_service(Service service)
+        {
+            int service_index = find_service(service);
+            if (service_index != -1)
+            {
+                return false;
+            }
+
+            services.Add(service);
+            return true;
+        }
+
+
+
     }
 }
